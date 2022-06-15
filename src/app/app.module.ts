f548f4c0b90572f2ce6144e0dc5c41d661554bed
Thomas Barré <thomasbarre07@gmail.com>
2022-06-15 09:51:18 +0200

    ajouter code footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { NavBarComponent } from './commons/nav-bar/nav-bar.component';
+import { NotFoundComponent } from './commons/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavBarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule
